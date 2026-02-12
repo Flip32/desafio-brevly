8b8707136a82bb40c70acac893f1e6f35a27dc2e
Flip32 <fl1p3r@gmail.com>
2026-02-12 18:54:12 -0300

    feat(server): routes and csv initial

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,16 @@
-import "dotenv/config";
-import {app} from "./http/app";
-import {env} from "./env";
+import 'dotenv/config'
+import { env } from './env'
+import { app } from './http/app'
 
 app
-    .listen({
-        port: env.PORT,
-        host: "0.0.0.0",
-    })
-    .then(() => {
-        app.log.info(`Server running on port ${env.PORT}`);
-    })
-    .catch((error) => {
-        app.log.error(error);
-        process.exit(1);
-    });
+  .listen({
+    port: env.PORT,
+    host: '0.0.0.0'
+  })
+  .then(() => {
+    app.log.info(`Server running on port ${env.PORT}`)
+  })
+  .catch(error => {
+    app.log.error(error)
+    process.exit(1)
+  })
